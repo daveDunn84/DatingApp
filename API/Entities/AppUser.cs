1afbdad4c0b9b67de0a2f2d880a7b7378940c0e7
Dave Dunn <63639593+daveDunn84@users.noreply.github.com>
2024-06-24 14:39:29 +0200

    Continuing work on this tutorial

---
+++ API/Entities/AppUser.cs
@@ -0,0 +1,7 @@
+﻿namespace API.Entities;
+
+public class AppUser
+{
+    public int Id { get; set; } // EF uses the field called "Id" as the primary key of the table
+    public string UserName { get; set; }
+}
